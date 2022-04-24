6505c38d3e3ef19b0d6da91296c9898fb5c0e908
Necro <Imhuman2506@gmail.com>
2022-04-24 10:34:44 +0700

    forgor

--- bedwars-plugin/src/main/java/com/andrei1058/bedwars/support/papi/PAPISupport.java
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/support/papi/PAPISupport.java
@@ -189,7 +189,7 @@ public class PAPISupport extends PlaceholderExpansion {
                         if (a.getStatus() == GameState.playing) {
                             ITeam bwt = a.getTeam(player);
                             if (bwt != null) {
-                                replay = bwt.getName().substring(0,1).toUpperCase()
+                                replay = bwt.getName().substring(0,1).toUpperCase();
                             }
                         }
                     } else {
@@ -203,7 +203,7 @@ public class PAPISupport extends PlaceholderExpansion {
                         if (a.getStatus() == GameState.playing) {
                             ITeam bwt = a.getTeam(player);
                             if (bwt != null) {
-                                replay = String.valueOf(bwt.getColor().chat()) + bwt.getName().substring(0,1).toUpperCase()
+                                replay = String.valueOf(bwt.getColor().chat()) + bwt.getName().substring(0,1).toUpperCase();
                             }
                         }
                     } else {
